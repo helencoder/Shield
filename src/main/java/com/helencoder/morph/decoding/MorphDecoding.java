a6463498485ccf29d988a01032c32156cb4f7884
helencoder <731873664@qq.com>
2018-01-03 19:43:04 +0800

    变形词生成&解析

---
+++ src/main/java/com/helencoder/morph/decoding/MorphDecoding.java
@@ -0,0 +1,9 @@
+package com.helencoder.morph.decoding;
+
+/**
+ * 变形词解析
+ *
+ * Created by helencoder on 2018/1/3.
+ */
+public class MorphDecoding {
+}
---
+++ src/main/java/com/helencoder/morph/encoding/MorphEncoding.java
@@ -0,0 +1,10 @@
+package com.helencoder.morph.encoding;
+
+/**
+ * 变形词生成
+ *
+ * Created by helencoder on 2018/1/3.
+ */
+public class MorphEncoding {
+
+}
