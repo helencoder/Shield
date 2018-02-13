06b2960c0866f84ce6a70c416854dfcee90224ac
helencoder <731873664@qq.com>
2018-02-13 09:46:19 +0800

    拼音、简繁转换

---
+++ src/main/java/com/helencoder/shield/word/PhoneticWord.java
@@ -0,0 +1,10 @@
+package com.helencoder.shield.word;
+
+/**
+ * 音近词(pinyin4j)
+ *
+ * Created by helencoder on 2018/1/10.
+ */
+public class PhoneticWord {
+
+}
---
+++ src/main/java/com/helencoder/shield/word/TraditionalWord.java
@@ -0,0 +1,10 @@
+package com.helencoder.shield.word;
+
+/**
+ * 简繁转换(ZHConverter)
+ *
+ * Created by helencoder on 2018/1/10.
+ */
+public class TraditionalWord {
+
+}
